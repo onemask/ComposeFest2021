87a20802de10071263f84ff5d886a487fa2e755c
soo <onemask@naver.com>
2021-11-12 00:53:16 +0900

    [jetpack-compose-basics] step-8

--- week 1-Jetpack Compose basics/BasicCodeLabPractice/app/src/main/java/com/soo/sample/basiccodelabpractice/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodeLabPractice/app/src/main/java/com/soo/sample/basiccodelabpractice/MainActivity.kt
@@ -3,10 +3,12 @@ package com.soo.sample.basiccodelabpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
@@ -14,6 +16,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,11 +35,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(names: List<String> = listOf("World", "Compose")) {
+  var shouldShowOnboarding = remember { mutableStateOf(true) }
+  if (shouldShowOnboarding.value) {
+    OnboardingScreen(onContinueClicked = { shouldShowOnboarding.value = false })
+  } else {
+    Greetings()
+  }
+/*
   Column(modifier = Modifier.padding(vertical = 4.dp)) {
     for (name in names) {
       Greeting(name = name)
     }
   }
+*/
 }
 
 @Composable
@@ -65,10 +76,47 @@ private fun Greeting(name: String) {
   }
 }
 
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+  Column(modifier = Modifier.padding(vertical = 4.dp)) {
+    for (name in names) {
+      Greeting(name = name)
+    }
+  }
+}
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
   BasicCodeLabPracticeTheme {
     MyApp()
+  }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+  // TODO: This state should be hoisted
+  Surface {
+    Column(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text("Welcome to the Basics Codelab!")
+      Button(
+        modifier = Modifier.padding(vertical = 24.dp),
+        onClick = onContinueClicked
+      ) {
+        Text("Continue")
+      }
+    }
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+  BasicCodeLabPracticeTheme {
+    OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
   }
 }
