773a4ef10e9b92cc0cfe9c98cafb77f06872083c
soo <onemask@naver.com>
2021-11-12 14:42:17 +0900

    step 3

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/soo/sample/JetpackComposeLayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/soo/sample/JetpackComposeLayout/MainActivity.kt
@@ -3,11 +3,15 @@ package com.soo.sample.JetpackComposeLayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
@@ -17,6 +21,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,8 +59,14 @@ fun PhotographerCardPreview() {
 }
 
 @Composable
-fun PhotographerCard() {
-  Row {
+fun PhotographerCard(modifier: Modifier = Modifier) {
+  Row(modifier
+    .padding(8.dp)
+    .clickable(onClick = { /* Ignoring onClick */ })
+    .clip(RoundedCornerShape(4.dp))
+    .background(MaterialTheme.colors.surface)
+    .padding(16.dp)
+  ) {
     Surface(
       modifier = Modifier.size(50.dp),
       shape = CircleShape,
