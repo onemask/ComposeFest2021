8f72dce806c693f1ac10628495ba6ed087601783
soo <onemask@naver.com>
2021-11-16 01:05:11 +0900

    step 7

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/soo/sample/JetpackComposeLayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/soo/sample/JetpackComposeLayout/MainActivity.kt
@@ -36,9 +36,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.soo.sample.JetpackComposeLayout.ui.theme.JetpackComposeLayoutTheme
@@ -49,7 +54,8 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       JetpackComposeLayoutTheme {
-        ScrollingList()
+       // ScrollingList()
+        BodyContent()
       }
     }
   }
@@ -74,14 +80,6 @@ fun LayoutsCodelab() {
     BodyContent(Modifier
       .padding(innerPadding)
       .padding(8.dp))
-  }
-}
-
-@Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-  Column(modifier = modifier.padding(8.dp)) {
-    Text(text = "Hi there!")
-    Text(text = "Thanks for going through the Layouts codelab")
   }
 }
 
@@ -214,6 +212,86 @@ fun ScrollingList() {
     LazyColumn(state = scrollState) {
       items(listSize) {
         ImageListItem(it)
+      }
+    }
+  }
+}
+
+fun Modifier.firstBaselineToTop(
+  firstBaselineToTop: Dp,
+) = this.then(
+  layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+      // Where the composable gets placed
+      placeable.placeRelative(0, placeableY)
+    }
+  }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+  JetpackComposeLayoutTheme {
+    Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+  }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+  JetpackComposeLayoutTheme {
+    Text("Hi there!", Modifier.padding(top = 32.dp))
+  }
+}
+
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+  MyOwnColumn(modifier.padding(8.dp)) {
+    Text("MyOwnColumn")
+    Text("places items")
+    Text("vertically.")
+    Text("We've done it by hand!")
+  }
+}
+
+@Composable
+fun MyOwnColumn(
+  modifier: Modifier = Modifier,
+  content: @Composable () -> Unit,
+) {
+  Layout(
+    modifier = modifier,
+    content = content
+  ) { measurables, constraints ->
+    // Don't constrain child views further, measure them with given constraints
+    // List of measured children
+    val placeables = measurables.map { measurable ->
+      // Measure each child
+      measurable.measure(constraints)
+    }
+
+    // Track the y co-ord we have placed children up to
+    var yPosition = 0
+
+    // Set the size of the layout as big as it can
+    layout(constraints.maxWidth, constraints.maxHeight) {
+      // Place children in the parent layout
+      placeables.forEach { placeable ->
+        // Position item on the screen
+        placeable.placeRelative(x = 0, y = yPosition)
+
+        // Record the y co-ord placed up to
+        yPosition += placeable.height
       }
     }
   }
