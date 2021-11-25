eea932a119451a96dd3fee12ebd898378696f989
soo <onemask@naver.com>
2021-11-25 19:51:13 +0900

    step 7

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -21,7 +21,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -33,6 +35,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -192,3 +195,41 @@ fun TodoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: M
   TodoInputText(text, onTextChange, modifier)
 }
 
+@Composable
+fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
+  val (text, setText) = remember { mutableStateOf("") }
+  val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default) }
+  val iconsVisible = text.isNotBlank()
+  val submit = {
+    onItemComplete(TodoItem(text, icon))
+    setIcon(TodoIcon.Default)
+    setText("")
+  }
+  Column {
+    Row(
+      Modifier
+        .padding(horizontal = 16.dp)
+        .padding(top = 16.dp)
+    ) {
+      TodoInputText(
+        text = text,
+        onTextChange = setText,
+        modifier = Modifier
+          .weight(1f)
+          .padding(end = 8.dp),
+        onImeAction = submit // pass the submit callback to TodoInputText
+      )
+      TodoEditButton(
+        onClick = submit, // pass the submit callback to TodoEditButton
+        text = "Add",
+        modifier = Modifier.align(Alignment.CenterVertically),
+        enabled = text.isNotBlank()
+      )
+    }
+    if (iconsVisible) {
+      AnimatedIconRow(icon, setIcon, Modifier.padding(top = 8.dp))
+    } else {
+      Spacer(modifier = Modifier.height(16.dp))
+    }
+  }
+}
