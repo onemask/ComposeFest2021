67ddbf910771c2167380cc39682bbc5c8c3051ec
soo <onemask@naver.com>
2021-12-05 17:54:07 +0900

    step 5

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -32,7 +32,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.compose.rally.data.Bill
+import androidx.navigation.navDeepLink
 import com.example.compose.rally.data.UserData.accounts
 import com.example.compose.rally.ui.accounts.AccountsBody
 import com.example.compose.rally.ui.accounts.SingleAccountBody
@@ -107,13 +107,15 @@ private fun RallyNavHost(
 
     val accountsName = RallyScreen.Accounts.name
     composable(
-      route = "$accountsName/{name}",
+      route = "${accountsName}/{name}",
       arguments = listOf(
         navArgument("name") {
-          // Make argument type safe
           type = NavType.StringType
-        }
-      )
+        },
+      ),
+      deepLinks = listOf(navDeepLink {
+        uriPattern = "rally//$accountsName/{name}"
+      })
     ) { entry -> // Look up "name" in NavBackStackEntry's arguments
       val accountName = entry.arguments?.getString("name")
       // Find first name match in UserData
