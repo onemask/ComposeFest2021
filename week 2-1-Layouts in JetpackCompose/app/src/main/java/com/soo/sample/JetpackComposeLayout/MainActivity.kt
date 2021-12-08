e0134cb5f80ae831314a8f344df2e662d706631d
soo <onemask@naver.com>
2021-12-09 01:10:47 +0900

    step 10

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/soo/sample/JetpackComposeLayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/soo/sample/JetpackComposeLayout/MainActivity.kt
@@ -491,3 +491,103 @@ fun ChipPreview() {
     Chip(text = "Hi there")
   }
 }
+@Composable
+fun ConstraintLayoutContent() {
+  ConstraintLayout {
+    // Creates references for the three composables
+    // in the ConstraintLayout's body
+    val (button1, button2, text) = createRefs()
+
+    Button(
+      onClick = { /* Do something */ },
+      modifier = Modifier.constrainAs(button1) {
+        top.linkTo(parent.top, margin = 16.dp)
+      }
+    ) {
+      Text("Button 1")
+    }
+
+    Text("Text", Modifier.constrainAs(text) {
+      top.linkTo(button1.bottom, margin = 16.dp)
+      centerAround(button1.end)
+    })
+
+    val barrier = createEndBarrier(button1, text)
+    Button(
+      onClick = { /* Do something */ },
+      modifier = Modifier.constrainAs(button2) {
+        top.linkTo(parent.top, margin = 16.dp)
+        start.linkTo(barrier)
+      }
+    ) {
+      Text("Button 2")
+    }
+  }
+}
+
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+  LayoutsCodelabTheme {
+    ConstraintLayoutContent()
+  }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+  ConstraintLayout {
+    val text = createRef()
+
+    val guideline = createGuidelineFromStart(fraction = 0.5f)
+    Text(
+      "This is a very very very very very very very long text",
+      Modifier.constrainAs(text) {
+        linkTo(start = guideline, end = parent.end)
+      }
+    )
+  }
+}
+
+@Preview
+@Composable
+fun LargeConstraintLayoutPreview() {
+  LayoutsCodelabTheme {
+    LargeConstraintLayout()
+  }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+  BoxWithConstraints {
+    val constraints = if (maxWidth < maxHeight) {
+      decoupledConstraints(margin = 16.dp) // Portrait constraints
+    } else {
+      decoupledConstraints(margin = 32.dp) // Landscape constraints
+    }
+
+    ConstraintLayout(constraints) {
+      Button(
+        onClick = { /* Do something */ },
+        modifier = Modifier.layoutId("button")
+      ) {
+        Text("Button")
+      }
+
+      Text("Text", Modifier.layoutId("text"))
+    }
+  }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+  return ConstraintSet {
+    val button = createRefFor("button")
+    val text = createRefFor("text")
+
+    constrain(button) {
+      top.linkTo(parent.top, margin= margin)
+    }
+    constrain(text) {
+      top.linkTo(button.bottom, margin)
+    }
+  }
+}
